38d581fc6ed60f05a7d469e8a7144011c5f09019
Ted <kimtx2017@gmail.com>
2017-07-04 21:35:44 +0900

    # Implements NPM Install
    - 외부 모듈 설치를 위한 작업 구현
    - npm install init : 외부모듈 리스트를 만들기 위한 package 폴더 생성
    - npm install '외부모듈명' : 'nconf' 와 같은 특정 모듈을 받아 올 수 있다.

--- JavaScript_Basic/module_01.js
+++ JavaScript_Basic/module_01.js
@@ -9,7 +9,8 @@ calc.add = function(a, b){
 // char 속성 정의
 calc.name = "ted";
 
+module.exports = calc.add;
 console.log(calc.name + " 모듈로 분리하기 전 상태 : " + calc.add(1, 6));
 
-module.exports = calc.add;
+
 
--- JavaScript_Basic/module_02.js
+++ JavaScript_Basic/module_02.js
@@ -1,3 +1,8 @@
 var adder = require("./module_01");
 
 console.log("모듈로 분리 한 뒤의 값 : " + adder(10,10));
+
+// 시스템 환경 변수 가져오기
+var nconf = require("nconf");
+var os = nconf.get("OS");
+console.log("OS 환경변수의 값 : " + os);
