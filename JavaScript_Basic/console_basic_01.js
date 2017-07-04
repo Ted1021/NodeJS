af8ee522cf7cb8b18e10be3516f8e5b97c859f49
Ted <kimtx2017@gmail.com>
2017-07-04 21:09:23 +0900

    # Representation of javaScript object
    - JS Object
    - JS Array

--- JavaScript_Basic/console_basic_01.js
+++ JavaScript_Basic/console_basic_01.js
@@ -1,1 +1,19 @@
 console.log("Hello World!! :)");
+
+// 객체의 표현
+var person = {
+    name : "ted",
+    age : 28
+}
+
+console.log("Get javaScript Object %j", person);
+console.dir(person);
+
+// 객체 배열의 표현
+var array = [
+    {testName : "ted", testAge : 28},
+    {testName : "tad", testAge : 21}
+];
+
+console.dir(array);
+console.log(array[0]);
